fffb93d6a22d0b7a9c706bb385f977fa7dffcb04
Daniel <daniel.onderk@gmail.com>
2023-11-25 22:30:54 +0100

    Add: Servo done, absolute, increment decrement limits check

--- src/adeept_awr_output_devices/adeept_awr_output_devices/servo_node.py
+++ src/adeept_awr_output_devices/adeept_awr_output_devices/servo_node.py
@@ -1,70 +1,105 @@
 import rclpy
-from rclpy.action import ActionServer
 from rclpy.node import Node
+from rclpy.action import ActionServer
 
 from adeept_awr_interfaces.action import Servo
 
-import Adafruit_PCA9685 # Import the library used to communicate with PCA9685
+import Adafruit_PCA9685
 import time
-
-
 
 class ServoNode(Node):
     def __init__(self):
         super().__init__("servo_node")
-        self.action_server = ActionServer(self, Servo, 'put_servo_to_pos', self.execute_callback)
+        self.action_server = ActionServer(self, Servo, 'put_servo_to_pos', self.action_callback)
 
         self.pwm = Adafruit_PCA9685.PCA9685()
         self.pwm.set_pwm_freq(50)
         self.curr_servo_pos = 300
 
+        self.get_logger().info('InitDone')
 
-    def execute_callback(self, goal_handle):
-        self.get_logger().info('Executing goal...')
 
+    def action_callback(self, goal_handle):
         feedback_msg = Servo.Feedback()
 
-        diff = self.curr_servo_pos - goal_handle.request.target_position
+        if goal_handle.request.mode == 0: #absolute pos
+            target_position = self.checkLimits(goal_handle.request.value)
 
-        if diff < 0:
-            for i in range(0,abs(diff)):
-                self.curr_servo_pos += 1
-                self.pwm.set_pwm(0, 0, (self.curr_servo_pos))
+            if self.curr_servo_pos < target_position:
+                for value in self.moveToPosUp(target_position):
+                    print(f"Received value: {value}")
+                    feedback_msg.curr_position = self.curr_servo_pos
+                    goal_handle.publish_feedback(feedback_msg)
+            else:
+                for value in self.moveToPosDown(target_position):
+                    print(f"Received value: {value}")
+                    feedback_msg.curr_position = self.curr_servo_pos
+                    goal_handle.publish_feedback(feedback_msg)
+        
+        elif goal_handle.request.mode == 1: # decrement (look up)
+            target_position = self.checkLimits(self.curr_servo_pos - goal_handle.request.value)
+            for value in self.moveToPosDown(target_position):
+                print(f"Received value: {value}")
                 feedback_msg.curr_position = self.curr_servo_pos
                 goal_handle.publish_feedback(feedback_msg)
-                time.sleep(0.05)
+
+        elif goal_handle.request.mode == 2: # increment (look down)
+            target_position = self.checkLimits(self.curr_servo_pos + goal_handle.request.value)
+            for value in self.moveToPosUp(target_position):
+                print(f"Received value: {value}")
+                feedback_msg.curr_position = self.curr_servo_pos
+                goal_handle.publish_feedback(feedback_msg)
+
         else:
-            for i in range(0,diff):
-                self.curr_servo_pos -= 1
-                self.pwm.set_pwm(0, 0, (self.curr_servo_pos))
-                feedback_msg.curr_position = self.curr_servo_pos
-                goal_handle.publish_feedback(feedback_msg)
-                time.sleep(0.1)
+            pass
 
         goal_handle.succeed()
-
         result = Servo.Result()
         result.end_position = self.curr_servo_pos
         return result
 
+    def checkLimits(self, target_position):
+        upper_limit = 150 #almost vertical
+        lower_limit = 350 #little below horizontal
 
-    def moveServo(self, goal_pos):
-        diff = self.curr_servo_pos - goal_pos
-
-        if diff < 0:
-            for i in range(0,diff):
-                self.curr_servo_pos += 1
-                self.pwm.set_pwm(3, 0, (self.curr_servo_pos))
-                time.sleep(0.05)
+        if target_position > lower_limit:
+            return lower_limit
+        elif target_position < upper_limit:
+            return upper_limit
         else:
-            for i in range(0,diff):
-                self.curr_servo_pos -= 1
-                self.pwm.set_pwm(3, 0, (self.curr_servo_pos))
-                time.sleep(0.05)
+            return target_position
 
+    def moveToPosUp(self, target_position):
+        for i in range(self.curr_servo_pos, target_position):
+            self.curr_servo_pos += 1
+            self.pwm.set_pwm(0, 0, (self.curr_servo_pos))
+            yield self.curr_servo_pos
+            time.sleep(0.05)
+
+    def moveToPosDown(self, target_position):
+        for i in range(self.curr_servo_pos, target_position, -1):
+            self.curr_servo_pos -= 1
+            self.pwm.set_pwm(0, 0, (self.curr_servo_pos))
+            yield self.curr_servo_pos
+            time.sleep(0.05)
+
+    def moveToDefaultPosition(self):
+        defaultServoPosition = 300
+        if self.curr_servo_pos < defaultServoPosition:
+            for value in self.moveToPosUp(defaultServoPosition):
+                pass
+        else:
+            for value in self.moveToPosDown(defaultServoPosition):
+                pass
 
 def main():
     rclpy.init()
     node = ServoNode()
-    rclpy.spin(node)
-    rclpy.shutdown()
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        node.moveToDefaultPosition()
+        node.destroy_node()
+        #rclpy.shutdown()
+    finally:
+        pass
