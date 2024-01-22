227986d45ab80b80c4bd689af4c3031e2d48e6e2
Daniel <daniel.onderk@gmail.com>
2024-01-22 16:12:15 +0100

    Change: line tracking now to topic

--- src/adeept_awr_input_sensors/adeept_awr_input_sensors/line_tracking_node.py
+++ src/adeept_awr_input_sensors/adeept_awr_input_sensors/line_tracking_node.py
@@ -4,6 +4,8 @@ from rclpy.node import Node
 import RPi.GPIO as GPIO
 import time
 
+from adeept_awr_interfaces.msg import LineTracking
+
 class LineTrackerNode(Node):
 
     def __init__(self):
@@ -16,7 +18,8 @@ class LineTrackerNode(Node):
         self.line_pin_middle = self.get_parameter('middle_pin').get_parameter_value().integer_value
         self.line_pin_right = self.get_parameter('right_pin').get_parameter_value().integer_value
         
-        self.timer = self.create_timer(1, self.run)
+        self.timer = self.create_timer(1, self.scanLine)
+        self.publisher = self.create_publisher(LineTracking, "line_visibility", 10)
 
         GPIO.setwarnings(False)
         GPIO.setmode(GPIO.BCM)
@@ -26,26 +29,28 @@ class LineTrackerNode(Node):
 
         self.get_logger().info("InitDone")
 
-    def run(self):
+    def scanLine(self):
         status_right = GPIO.input(self.line_pin_right)
         status_middle = GPIO.input(self.line_pin_middle)
         status_left = GPIO.input(self.line_pin_left)
 
-        if status_middle == 1:
-            self.get_logger().info("middle")
-        if status_left == 1:
-            self.get_logger().info("left")
-        if status_right == 1:
-            self.get_logger().info("right")
+        msg = LineTracking()
+        msg.middle = False
+        msg.left = False
+        msg.right = False
 
-        self.get_logger().info("----------")
+        if status_middle == 1:
+            msg.middle = True
+        if status_left == 1:
+            msg.left = True
+        if status_right == 1:
+            msg.right = True
+
+        self.publisher.publish(msg)
+
 
 def main():
     rclpy.init()
     node = LineTrackerNode()
-    
-    while 1:
-        node.run()
-        time.sleep(1)
-
+    rclpy.spin(node)
     rclpy.shutdown()
