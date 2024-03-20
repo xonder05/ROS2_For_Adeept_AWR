201a3c2c047ae1f8966387357f4e6777e4a829db
xonder05 <daniel.onderk@gmail.com>
2024-03-20 16:40:06 +0000

    Update: imu node features

--- src/adeept_awr_nodes/adeept_awr_nodes/imu_node.py
+++ src/adeept_awr_nodes/adeept_awr_nodes/imu_node.py
@@ -21,10 +21,10 @@ class ImuNode(Node):
         )
         self.scanning_period = self.get_parameter('scanning_period').get_parameter_value().double_value
         
-        self.subscriber = self.create_subscription(Twist, "/drive_directions", self.twist_callback, 10)
+        self.subscriber = self.create_subscription(Twist, "/cmd_vel", self.twist_callback, 10)
         self.publisher = self.create_publisher(Twist, "/imu_node/sensor_reading", 10)
         self.colision_publisher = self.create_publisher(Bool, "/imu_node/colision_warning", 10)
-        self.timer = self.create_timer(0.005, self.publish_tf_data)
+        self.tf_timer = self.create_timer(0.01, self.publish_tf_data)
         
         self.sensor = sensor = mpu6050(0x68)
         sensor.set_filter_range(0x05)
@@ -39,10 +39,11 @@ class ImuNode(Node):
         self.prev_time = self.get_clock().now()
         self.prev_accel_data = {'x': 0.0, 'y': 0.0, 'z': 0.0}
         self.prev_velocity = {'x': 0.0, 'y': 0.0, 'z': 0.0}
-        self.prev_position = {'x': 0.0, 'y': 0.0, 'z': 0.0}
         self.orientation = {'roll': 0.0, 'pitch': 0.0, 'yaw': 0.0}
 
         self.absolute_position = {'x': 0.0, 'y': 0.0}
+
+        self.max = 0
 
         self.get_logger().info("InitDone")
 
@@ -50,26 +51,24 @@ class ImuNode(Node):
         self.linear_command = msg.linear.x
         self.angular_command = msg.angular.z
 
-    def publish_tf_data(self):
-        current_time = self.get_clock().now()
-        dt = (current_time - self.prev_time).nanoseconds / 1e9
-        
+
+    def get_accel_data(self):
         accel_data = {'x': 0.0, 'y': 0.0, 'z': 0.0}
-        for i in range(0, 10):
         
+        #average value from x samples
+        for i in range(0, 1):
             new_data = self.sensor.get_accel_data()
 
             accel_data['x'] += (new_data['x'] - 0.165931074)
             accel_data['y'] += (new_data['y'] + 10.001548310205276)
             accel_data['z'] += (new_data['z'] - 0.141471371)
 
-        accel_data['x'] = accel_data['x'] / 10.0
-        accel_data['y'] = accel_data['y'] / 10.0
-        accel_data['z'] = accel_data['z'] / 10.0
+        accel_data['x'] = accel_data['x'] / 1.0
+        accel_data['y'] = accel_data['y'] / 1.0
+        accel_data['z'] = accel_data['z'] / 1.0
 
-        accel_data['x'] *= -1
-
-        if abs(accel_data['x']) < 0.1:
+        #cut low values (so the robot does not drift while stationary)
+        if abs(accel_data['x']) < 0.15:
             accel_data['x'] = 0
 
         if abs(accel_data['y']) < 0.05:
@@ -78,17 +77,31 @@ class ImuNode(Node):
         if abs(accel_data['z']) < 0.5:
             accel_data['z'] = 0
 
+        #align physical axis with those used by ros2
+        accel_data['x'] *= -1
+
         tmp = accel_data['y']
         accel_data['y'] = accel_data['z']
         accel_data['z'] = tmp
 
+        return accel_data
 
-        gyro_data_deg = self.sensor.get_gyro_data()
+    def get_gyro_data(self):
+        gyro_data_deg = {'x': 0.0, 'y': 0.0, 'z': 0.0}
 
-        gyro_data_deg['x'] -= -1.7608719847328245
-        gyro_data_deg['y'] -= -0.4370078625954107
-        gyro_data_deg['z'] += 0.10326725190839636
+        #average value from x samples
+        for i in range(0, 1):
+            new_data = self.sensor.get_gyro_data()
 
+            gyro_data_deg['x'] += (new_data['x'] - -1.7608719847328245)
+            gyro_data_deg['y'] += (new_data['y'] - -0.4370078625954107)
+            gyro_data_deg['z'] += (new_data['z'] + 0.10326725190839636)
+
+        gyro_data_deg['x'] = gyro_data_deg['x'] / 1.0
+        gyro_data_deg['y'] = gyro_data_deg['y'] / 1.0
+        gyro_data_deg['z'] = gyro_data_deg['z'] / 1.0
+
+        #cut low values (so the robot does not drift while stationary)
         if abs(gyro_data_deg['x']) < 5:
             gyro_data_deg['x'] = 0
 
@@ -98,33 +111,63 @@ class ImuNode(Node):
         if abs(gyro_data_deg['z']) < 5:
             gyro_data_deg['z'] = 0
 
+        #align physical axis with those used by ros2
         tmp = gyro_data_deg['y']
         gyro_data_deg['y'] = gyro_data_deg['z']
         gyro_data_deg['z'] = tmp
 
+        #convertion from degrees to radians
         gyro_data = {
             'x': math.radians(gyro_data_deg['x']),
             'y': math.radians(gyro_data_deg['y']),
             'z': math.radians(gyro_data_deg['z'])
         }
 
-        for axis in ['x', 'y', 'z']:
-            self.prev_velocity[axis] += (accel_data[axis] + self.prev_accel_data[axis]) / 2 * dt
-            self.prev_position[axis] += self.prev_velocity[axis]
+        return gyro_data
 
-        # Calculate change in position based on current linear velocity and orientation
+    def publish_tf_data(self):
+        
+        #get data
+        current_time = self.get_clock().now()
+        dt = (current_time - self.prev_time).nanoseconds / 1e9
+    
+        accel_data = self.get_accel_data()
+
+        gyro_data = self.get_gyro_data()
+
+        #calculate velocities, rotations and positions
+        self.prev_velocity['x'] += (accel_data['x'] + self.prev_accel_data['x']) / 2 * dt
+
         delta_x = self.prev_velocity['x'] * math.cos(self.orientation['yaw']) * dt
         delta_y = self.prev_velocity['x'] * math.sin(self.orientation['yaw']) * dt
 
-        # Update absolute x and y positions
         self.absolute_position['x'] += delta_x
         self.absolute_position['y'] += delta_y
 
-        # if (abs(accel_data['x']) > 0 or abs(self.prev_accel_data['x']) > 0):
-            # self.get_logger().info(f"{self.prev_velocity['x']} = {(accel_data['x'] + self.prev_accel_data['x']) / 2 * dt}")
-
         self.orientation['yaw'] += gyro_data['z'] * dt
+
+        #publish imu data
+        msg = Twist()
+        msg.linear.x = self.prev_velocity['x']
+        msg.angular.z = self.orientation['yaw']
+        self.publisher.publish(msg)
+
+        #publish not moving warning
+        if accel_data['x'] < 0.1 and (self.linear_command - self.prev_velocity['x']) > 0.2:
+            if self.strike:
+                msg = Bool()
+                msg.data = True
+                self.publisher.publish(msg)
+            else:
+                self.strike = True
+        else:
+            self.strike = False
+
+            msg = Bool()
+            msg.data = False
+            self.publisher.publish(msg)
         
+        #publish transform
         transform = TransformStamped()
         transform.header.stamp = current_time.to_msg()
         transform.header.frame_id = 'odom'
@@ -138,7 +181,7 @@ class ImuNode(Node):
         self.prev_time = current_time
         self.prev_accel_data = accel_data
 
-        if abs(self.prev_velocity['x']) < 0.1:
+        if abs(self.prev_velocity['x']) < 0.01:
             self.prev_velocity['x'] = 0
 
     def euler_to_quaternion(self, roll, pitch, yaw):
@@ -154,46 +197,7 @@ class ImuNode(Node):
         qy = sy * cp * sr + cy * sp * cr
         qz = sy * cp * cr - cy * sp * sr
 
-        # self.get_logger().info(f"{sy}*{cp}*{cr}-{cy}*{sp}*{sr}")
-        # self.get_logger().info(f"x: {qx}, y: {qy}, z:{qz}, w: {qw}")
-
         return Quaternion(x=qx, y=qy, z=qz, w=qw)
-
-
-    def timer_callback(self):
-        msg = Twist()
-        
-        x = 0
-        for i in range(0,10):
-            x = x + self.sensor.get_accel_data()['x']
-        accel_data = x / 10.0
-
-        msg.linear.x = accel_data
-        msg.angular.z = self.sensor.get_gyro_data()['y']
-        self.publisher.publish(msg)
-
-        msg = Bool()
-
-        maximum = 0
-        for item in self.history:
-            maximum = max(abs(item), maximum)
-
-        if maximum < 0.5 and self.linear_command == 0:
-            msg.data = False #calm
-
-        elif maximum < 0.5 and self.linear_command > 0:
-            msg.data = True #stopped wheels not spining
-
-        # elif maximum < 1.75 and self.linear_command > 0:
-        #     msg.data = True #stopped wheels spining
-
-        else:
-            self.data = False #driving
-
-        self.colision_publisher.publish(msg)
-
-        self.history.append(accel_data)
-        self.history.pop(0)
 
 def main():
     rclpy.init()
