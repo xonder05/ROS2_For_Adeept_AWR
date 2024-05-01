da3aeb97f6dc7b9350d7ca0f6c39b4fc999af4cd
xonder05 <daniel.onderk@gmail.com>
2024-05-01 13:36:16 +0200

    Fix: rviz launch

--- src/controllers/launch/rviz_launch.py
+++ src/controllers/launch/rviz_launch.py
@@ -1,9 +1,16 @@
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 import os
 
 def generate_launch_description():
+
+    use_sim_time_arg = DeclareLaunchArgument(
+        'use_sim_time',
+        default_value='false',
+    )
 
     rviz_config_path = os.path.join(
         get_package_share_directory('controllers'),
@@ -16,10 +23,11 @@ def generate_launch_description():
         executable='rviz2',
         arguments=['-d', rviz_config_path],
         parameters=[
-            {'use_sim_time': True}
+            {'use_sim_time': LaunchConfiguration('use_sim_time')}
         ],
     )
 
     return LaunchDescription([
+        use_sim_time_arg,
         rviz,
     ])
